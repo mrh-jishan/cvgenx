266f247a9cd9882d2c919e477f7cdb3aaa3a1979
robin-hassan <robiul.hassan12102@gmail.com>
2025-07-06 19:11:15 -0400

    fixed release

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: [
+    "main",
+    { name: 'beta', prerelease: true }
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git"
+  ]
+};
