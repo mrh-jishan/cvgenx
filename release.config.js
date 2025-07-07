7c1693891d6aa1af125640e60dbbe5f4860108a0
robin-hassan <robiul.hassan12102@gmail.com>
2025-07-06 20:08:40 -0400

    feat(config): allow npm publish true

--- release.config.js
+++ release.config.js
@@ -6,7 +6,12 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        "@semantic-release/npm",
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: true,
+            }
+        ],
         "@semantic-release/github",
         "@semantic-release/git"
     ]
